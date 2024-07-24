9e5dca002693616933cb33e960650d9603f18484
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-07-25 04:09:55 +0800

    西元2024年07月25日 (週四) 04時09分55秒 CST

---
+++ docker/restartup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the directory path of the script
+script_dir=$(dirname "$0")
+
+# Change the current working directory to the script's location
+cd "$script_dir"
+
+docker-compose down
+docker-compose up
---
+++ docker/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the directory path of the script
+script_dir=$(dirname "$0")
+
+# Change the current working directory to the script's location
+cd "$script_dir"
+
+docker-compose up
